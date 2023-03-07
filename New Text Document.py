d34f3097713fed261a7309b2896c398f56daaf55
AyyappaAndy <ayyappa54143@gmail.com>
2023-03-07 15:03:57 +0530

    project file added

---
+++ New Text Document.py
@@ -0,0 +1,5 @@
+1   li = [1,2,3,4,5]
+2   squares = [i**2 for i in li]
+3   cube = [i**3 for i in li]
+4   print("the square of the numbers are", squares)
+5   print("the cube of the numbers are", cube)
